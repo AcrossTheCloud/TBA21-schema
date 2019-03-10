f19aa584fe06c96a7e8ab51d86a061c0d26a02b9
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2019-03-11 10:33:00 +1100

    first go at the tba21 schema

---
+++ schema.sql
@@ -0,0 +1,25 @@
+/* Schema for TBA21 image/video */
+
+-- Schema name
+CREATE SCHEMA tba21;
+
+-- Geospatial support
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+
+-- Cryptographic support
+CREATE extension pgcrypto;
+
+-- Events table
+CREATE TABLE tba21.imagesmetadata
+(
+  id bigserial PRIMARY KEY,
+  decodedSrcKey varchar,
+  created_at timestamp with time zone NOT NULL,
+  updated_at timestamp with time zone NOT NULL,
+  metadata jsonb NOT NULL
+);
+
+-- Events geometry
+SELECT AddGeometryColumn ('tba21','imagesmetadata','the_geom',4326,'POINT',2);
+ALTER TABLE tba21.images ALTER COLUMN the_geom SET NOT NULL;
