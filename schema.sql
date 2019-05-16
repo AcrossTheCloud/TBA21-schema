b8ef09058cccccf7ae67fa9faee1d2ece0cf93c2
Matthew Berryman <matthew@acrossthecloud.net>
2019-05-16 16:37:37 +1000

    some tidy up and renaming

--- schema.sql
+++ schema.sql
@@ -21,13 +21,12 @@ CREATE TYPE tba21.licence_type AS ENUM ('CC BY', 'CC BY-SA', 'CC BY-ND', 'CC BY-
 CREATE TYPE tba21.gender AS ENUM ('male', 'female', 'other');
 
 -- Items metadata table
-CREATE TABLE tba21.itemsmetadata
+CREATE TABLE tba21.items
 (
 	sha512 varchar(128) PRIMARY KEY,
 	decodedSrcKey varchar,
 	created_at timestamp with time zone NOT NULL,
 	updated_at timestamp with time zone NOT NULL,
-	metadata jsonb NOT NULL,
 	time_produced timestamp with time zone,
 	concept_tags bigserial,
 	keyword_tags bigserial,
@@ -50,7 +49,7 @@ CREATE TABLE tba21.itemsmetadata
 );
 
 --People metadata table
-CREATE TABLE tba21.peoplemetadata
+CREATE TABLE tba21.people
 (
 	ID bigserial,
 	family_name varchar(128),
@@ -111,12 +110,12 @@ CREATE TABLE tba21.keyword_tags
 );
 
 -- Events geometry
-SELECT AddGeometryColumn ('tba21','itemsmetadata','the_geom',4326,'POINT',2);
+SELECT AddGeometryColumn ('tba21','items','the_geom',4326,'POINT',2);
 -- ALTER TABLE tba21.itemsmetadata ALTER COLUMN the_geom SET NOT NULL;
 
 --Collections and collections_items joins
 SELECT tba21.collections_items.ID, tba21.collections.ID FROM tba21.collections_items INNER JOIN tba21.collections ON tba21.collections_items.ID = tba21.collections.ID;
 
 -- People and collections joins
-SELECT tba21.collections_people.ID, tba21.tba21.peoplemetadata.ID FROM tba21.collections_people INNER JOIN tba21.tba21.peoplemetadata ON tba21.collections_people.ID = tba21.peoplemetadata.ID;
-SELECT tba21.collections_people.sha512, tba21.itemsmetadata.sha512 FROM tba21.collections_people INNER JOIN tba21.itemsmetadata ON tba21.collections_people.sha512 = tba21.itemsmetadata.sha512;
+SELECT tba21.collections_people.ID, tba21.tba21.people.ID FROM tba21.collections_people INNER JOIN tba21.tba21.people ON tba21.collections_people.ID = tba21.people.ID;
+SELECT tba21.collections_people.sha512, tba21.items.sha512 FROM tba21.collections_people INNER JOIN tba21.items ON tba21.collections_people.sha512 = tba21.items.sha512;
