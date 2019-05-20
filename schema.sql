04b9d7741d0d46df676f9e733cc599b5c61a23f4
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2019-05-20 14:45:29 +1000

    split references and other fixes

--- schema.sql
+++ schema.sql
@@ -21,18 +21,30 @@ CREATE TYPE tba21.licence_type AS ENUM ('CC BY', 'CC BY-SA', 'CC BY-ND', 'CC BY-
 CREATE TYPE tba21.gender AS ENUM ('male', 'female', 'other');
 
 -- Items metadata table
+CREATE TABLE tba21.s3uploads
+(
+	sha512 varchar(128) PRIMARY KEY,
+	all_s3_keys varchar[] , --all duplicate keys, if any, go here
+	created_at timestamp with time zone NOT NULL,
+	updated_at timestamp with time zone NOT NULL,
+    exif jsonb, -- for things that don't go into other columns
+	machine_recognition_tags varchar[],
+	other_metadata jsonb -- anything else like image-hash etc ? could decompose them here too 
+);
+
+-- Items metadata table
 CREATE TABLE tba21.items
 (
-	sha512 varchar(128),
-	s3_key varchar PRIMARY KEY,
+	ID bigserial PRIMARY KEY,
+	s3uploads_sha512 varchar(128)  references s3uploads(sha512),
+	s3_key varchar , --s3 key, if any, associated to this particular item
 	created_at timestamp with time zone NOT NULL,
 	updated_at timestamp with time zone NOT NULL,
 	time_produced timestamp with time zone,
 	status boolean,
-	exif jsonb, -- for things that don't go into other columns
 	concept_tags bigint[],
 	keyword_tags bigint[],
-	recognition_tags varchar(128),
+	recognition_tags varchar[],
 	place varchar(128),
 	country_or_ocean varchar(128),
 	creators varchar(256)[],
@@ -60,7 +72,7 @@ CREATE TABLE tba21.types
 --Collections metadata
 CREATE TABLE tba21.collections
 (
-	id bigserial PRIMARY KEY,
+	ID bigserial PRIMARY KEY,
 	s3_prefix varchar,
 	created_at timestamp with time zone NOT NULL,
 	updated_at timestamp with time zone NOT NULL,
@@ -87,6 +99,10 @@ CREATE TABLE tba21.collections
 );
 
 -- Geo stuff
+
+SELECT AddGeometryColumn ('tba21','s3uploads','location',4326,'POINT',2); -- s3uploads location column
+CREATE INDEX s3uploads_gix ON tba21.s3uploads USING GIST (location); -- s3uploads location GIST index
+
 SELECT AddGeometryColumn ('tba21','items','location',4326,'POINT',2); -- items location column
 CREATE INDEX items_gix ON tba21.items USING GIST (location); -- items location GIST index
 
@@ -96,8 +112,8 @@ CREATE INDEX items_gix ON tba21.collections USING GIST (location); -- collection
 -- Collection items cross-references
 CREATE TABLE tba21.collections_items 
 (
-	ID bigint references collections(id),
-	sha512 varchar(128) references items(id)
+	collection_ID bigint references collections(ID),
+	item_ID bigint references items(ID)
 );
 
 --Concept tags metadata
