125a421b26e8de83a02d62d423c812a219b1cf1a
Dan Wood <daniel@acrossthecloud.net>
2019-06-27 09:35:45 +1000

    Missed an s3_prefix in seeds.

--- seeds/1.collections.sql
+++ seeds/1.collections.sql
@@ -50,7 +50,7 @@ VALUES (
   'The Decisive Moment',
   'Description of The Decisive Moment. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non eros pulvinar tortor auctor tincidunt congue quis sem. Donec id sem non neque tincidunt mollis ac sed nulla.',
   ST_GeomFromText('LINESTRING(-71.160281 42.258729,-71.160837 42.259113,-71.161144 42.25932)', 4326)
-), ( 'NMnrIlBzBZxaKvnJZBO32jQpFQIDodOTgBwRuPeFs9XYVGj8h8chmEZUSGygOdkSGLucpRiO46owykUxPruwVllVW2jGO0e9Wu3M4hTRa2G5mrxRrORMPANKFS2fU4drhjhoQxDo8Lld4hC3B9WMBgzkLt5YGvif372ktvSGxMy0LnKZUsZuidkrPxjeEwYxMIcQyqxYcf3w5ceXxg77FPjcqSkFnwYqDdbXLlQL6Zc3E1QXVt2P2UJSbGWnX6hqYnnEE8Qh1dpJIoEifUvHPNazEMizZpVZYTMA1wsz2sejM1fTdz466bGFlYJqe5LHLX3paYgu0Bv97L7Gzm2e0Phm9LNPQiRj9Xf0vNgw3jPGfHNvN8nZ019kcKMv4y79C3cbjHYm4dujRvOoLMWzb6XWyU60iDaye7NFiB8UWqwsUVMXYU2bZhVRjsGB30xxAoilQusHO9GGmnw78sp6kQP0YzggT2f9oXUTp9UZfxcUeLJ4PmQdWaHChBqEH5d9WoqeG2kH5BVPJl33fo9TUqc0IZu4oepZ55BuQwpZflIu6KWWpr24NUqsZecbZyxlgKR28Nvu7QW8uBRje0KESqwgklvRemyctuPqhnPZSlTy0EHc5421mU0rXObN82eUxYAJpxHDqamm9T5NJh6Mhn0p9wJyHEe7JbL1FZ32cVV1CCZG9b7x77JIkEiCxxfRJHLEdv0Fo7mYjxbqQFUk1VsNcANJKC5fpR1nU1e2AW1AW41FKlYSbURdK8BFhwYQTcq32Xhk9ouMZkh8dHOZxkPpsTiBZNsMZ6bGC5xXR664MQ3xn3rOuCjuSMjZiW2TmD2eNRUhz7u7gGqwNPIacWBYurAd919GuSmUc3HzqQxj4tWwUzpAlHgNnbDd3ekGMF5S3XQqhIgaSsHcHgm6fNWNQbi5RKgwYUwu5vmWMWF6hCzwrigcuPPndfjJpIINCx4ulRtLY814qPPYrYfwjYFCKI1rPkmQVFU',
+), (
   '2011-07-01 06:30:30+05',
   '2011-07-01 06:30:30+05',
   '2011-07-01 06:30:30+05',
