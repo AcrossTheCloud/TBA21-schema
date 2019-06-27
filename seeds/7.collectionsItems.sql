f8fd629905fc3e27ccb0f257ca51e580c2cd93f1
Zara <zara@acrossthecloud.net>
2019-06-28 09:52:29 +1000

    More items added to collections_items -- fix

--- seeds/7.collectionsItems.sql
+++ seeds/7.collectionsItems.sql
@@ -1,5 +1,5 @@
 -- START FIRST QUERY tba21.collection_items
 INSERT INTO tba21.collections_items(
 collection_ID, item_s3_key)
-VALUES(1, 'private/user/key1', 'private/user/key6','private/user/key7' ), (2, 'private/user/key2'), (3, 'private/user/key3', 'private/user/key4' );
+VALUES(1, 'private/user/key1'), (1, 'private/user/key6'),(1, 'private/user/key7' ), (2, 'private/user/key2'), (3, 'private/user/key3'),(3, 'private/user/key4' );
 --END FIRST QUERY tba21.collection_items
